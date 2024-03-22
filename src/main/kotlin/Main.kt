a82bfc7d131d17acdee989d21332a0abc54d153f
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-03-22 19:09:00 -0300

    feat: operadores lógicos matematicos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,5 +5,14 @@ fun main() {
     val numero: Long = 1_000_000_000 // posso representar 1M assim. Também é um padrão para representar esses numeros grander
     println(numero)
 
+    // operadores logicos numericos
+    var idade = 30
+    idade = idade + 10
+    println(idade)
+    println(idade - 10)
+    println(idade * 2) // multiplicaçao
+    println(idade / 2) // divisão
+    println(idade % 2) // resto
+
 
 }
