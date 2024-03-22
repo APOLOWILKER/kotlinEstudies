1b86202c5b9c5dcb01d08ad1ce2732ce246c0a37
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-03-22 16:25:16 -0300

    alocando espaços na memoria

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,25 +12,24 @@ fun main() {
     // TIPOS PRIMITIVOS
 
 
-    // ESPAÇOS DE MEMORIA EM TIPOS PRIMTIVOS
-
-    val price: Double = 9100.90
-    val byte: Byte = 8 // 8-Bit
-    val short: Short = 16 // 16-bit
-    val age: Int = 30 // 32-Bit (99% do tempo)
-    val long = 192031982309 // 64bit
-
     // Mantendo/ reservando todo o espaço na memoria.
     val memorySafe = 192L // 64bit reservo todos os 64bit da memoria.
 
-    var product: String = "macbook"
+    // ESPAÇOS DE MEMORIA EM TIPOS PRIMTIVOS
+
+    val byte: Byte = 8            // 8-bit
+    val short: Short = 16        // 16-bit
+    val age: Int = 31            // 32-bit (99% do tempo)
+    val long: Long = 192         // 64-bit
+
+    val desconto: Float = 31.90f // 32-bit
+    val price: Double = 331.90   // 64-bit ponto flutuante (floating point)
+
+    val product: String = "Notebook"
 
 
 
-    println("Você que um $product ?")
-    println("Sabia que o valor de um $product é $ $price")
-    println(price::class)
-    println(age::class)
-    println(product::class)
-    print(long::class)
+    println(desconto::class)
+    println(price2::class)
+
 }
