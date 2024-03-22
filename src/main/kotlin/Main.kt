57afdd6615b4f30d09b3c92e5dc740be378a691d
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-03-22 18:46:33 -0300

    padrão para representar big number in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ fun main() {
 
     // GLOSSARIO (var, val, fun, class, double, string, int, false, true)
     // NUMEROS GRANDES - tem 64 bits
-    val numero: Long = 1_000_000_000 // posso representar 1M assim.
+    val numero: Long = 1_000_000_000 // posso representar 1M assim. Também é um padrão para representar esses numeros grander
     println(numero)
 
 
