31d56f0a7ba8bf342e2c1a56355213810c2feff1
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-03-22 18:45:28 -0300

    representacao 1m no kotlin com underscore

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ fun main() {
 
     // GLOSSARIO (var, val, fun, class, double, string, int, false, true)
     // NUMEROS GRANDES - tem 64 bits
-    val numero: Long = 321616546843618465
+    val numero: Long = 1_000_000_000 // posso representar 1M assim.
     println(numero)
 
 
