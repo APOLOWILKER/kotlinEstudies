a06d0e591b00217331a474d290f05c8e676c9ad1
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-03-22 18:44:26 -0300

    numeros grandes no kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
 fun main() {
 
     // GLOSSARIO (var, val, fun, class, double, string, int, false, true)
-    val int = "10a" // isso não funciona.
-    println(int.toLong())
+    // NUMEROS GRANDES - tem 64 bits
+    val numero: Long = 321616546843618465
+    println(numero)
+
+
 }
