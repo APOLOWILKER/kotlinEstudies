f10c3d5431d0977083f24bf5532683a048b431bf
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-03-22 22:52:09 -0300

    type function and returns

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,24 @@ fun main() {
     // GLOSSARIO (var, val, fun, class, double, string, int, false, true, null, ?:)
 
     // functions - blocos de códigos reutilizáveis | Organizar código
+    // E elea pode ter um tipo de retorno (Double String Int Long) caso n tenha retorno ()
 
     val resposta = somar() // call
 
     // usa série de códigos
 
     println(resposta)
+    olaMundo()
 }
 
-fun somar(): Int {
-    return 2 + 3
+// funcao sem retorno igual a main
+fun olaMundo() {
+    println("olá Mundo")
+}
+
+// funcao com retorno  (String)
+fun somar(): String {
+    // por padrão o tipo Unit é omitido não precisa ser chamado
+    val resultado = 2 + 3
+    return "Estou somando 2 com 3 e o resultao é: $resultado"
 }
