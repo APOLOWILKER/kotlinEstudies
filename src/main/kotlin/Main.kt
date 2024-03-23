1fe59011223f1ccceb76b6e8ed1ded6bab69814f
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-03-22 23:21:51 -0300

    named arguments in fucntions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,21 +6,27 @@ fun main() {
     // E elea pode ter um tipo de retorno (Double String Int Long) caso n tenha retorno ()
     // escopo de funções
     // declaração e o corpo da função
-    olaMundo()
-    functionEnxuta()
-    ageValidate(18)
+//    olaMundo()
+//    functionEnxuta()
+    // Parametros NOMEADOS
+
+    ageValidate(age = 30, name = "Apolo", isAdmin = true) // argumentos nomeados
+    ageValidate(name = "Huge", isAdmin = false, age=27) // Posso reordenar os argumentos nomeados
+    ageValidate(17, "Bruce", false) // argumentos passados normalmente
 }
 
-fun olaMundo() {
-    println("olaMundo")
-    println("OlaMundo 02 \n")
-}
-
-fun functionEnxuta() = println("Se tem uma linha não precisa de chaves")
-
-fun ageValidate(age: Int) {
+//fun olaMundo() {
+//    println("olaMundo")
+//    println("OlaMundo 02 \n")
+//}
+//
+//fun functionEnxuta() = println("Se tem uma linha não precisa de chaves")
+// a função recebe parametros
+fun ageValidate(age: Int, name: String, isAdmin: Boolean) {
+    println("Minha idade é $age e meu nome é $name, e sou admin: $isAdmin!")
     if (age >= 18) {
-        println("\n Pode beber! \n")
+        println("\n E já posso beber! \n")
+    } else {
+        println("\n E ainda não posso beber!\n")
     }
-    println("\n Não pode beber!\n")
 }
