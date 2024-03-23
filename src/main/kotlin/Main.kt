c9bcdfdd1437a23b6a7c89925e05bc429e81af20
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-03-22 23:03:10 -0300

    declaration and body of a function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,34 +4,24 @@ fun main() {
 
     // functions - blocos de códigos reutilizáveis | Organizar código
     // E elea pode ter um tipo de retorno (Double String Int Long) caso n tenha retorno ()
-
-    val resposta = somar() // call
-
-    // usa série de códigos
-
-    println(resposta)
+    // escopo de funções
+    // declaração e o corpo da função
     olaMundo()
+    functionEnxuta()
+    ageValidate()
 }
 
-// funcao sem retorno igual a main
 fun olaMundo() {
-    println("olá Mundo")
-
-    // funções de escopo (RARO de Usar)
-    fun subtracao() {
-        println("uma mensagem para o usuário  informando que comecou a execução")
-        println(2+4)
-    }
-    subtracao()
-
-    // fazer várias coisas (login, registro no banco)
-
-    subtracao()
+    println("olaMundo")
+    println("OlaMundo 02 \n")
 }
 
-// funcao com retorno  (String)
-fun somar(): String {
-    // por padrão o tipo Unit é omitido não precisa ser chamado
-    val resultado = 2 + 3
-    return "Estou somando 2 com 3 e o resultao é: $resultado"
+fun functionEnxuta() = println("Se tem uma linha não precisa de chaves")
+
+fun ageValidate() {
+    val age = 31 // poderia ser um parametro
+    if (age > 18) {
+        println("Pode beber")
+    }
+    println("Não pode beber")
 }
