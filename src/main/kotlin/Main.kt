8e60f2204fd22e15573ff91f8870b2bfe562eede
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-03-22 23:39:51 -0300

    default values in functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,24 +10,14 @@ fun main() {
 //    functionEnxuta()
     // Parametros NOMEADOS
 
-    verificacaoParaLogin(age = 30, name = "Apolo", isAdmin = true) // argumentos nomeados
-    verificacaoParaLogin(name = "Huge", isAdmin = false, age=27) // Posso reordenar os argumentos nomeados
-    verificacaoParaLogin(17, "Bruce", false) // argumentos passados normalmente
-    verificacaoParaLogin(15, "Arthur") // function sobrecarregada
+    cadastroDeUsuario(name = "Bruce", age = 18)
+    cadastroDeUsuario(22, "Amy", false)
+    cadastroDeUsuario(name = "Tiago", age = 26)
+    cadastroDeUsuario(name = "Apolo", age = 30, isAdmin = true)
 }
 
-//fun olaMundo() {
-//    println("olaMundo")
-//    println("OlaMundo 02 \n")
-//}
-//
-//fun functionEnxuta() = println("Se tem uma linha não precisa de chaves")
 // a função recebe parametros
-fun verificacaoParaLogin(age: Int, name: String, isAdmin: Boolean) {
-    println("Minha idade é $age e meu nome é $name, e sou admin: $isAdmin!")
-}
-
-// sobrecarga de metodo
-fun verificacaoParaLogin(age: Int, name: String) {
-    println("Instrução 2: Minha idade é $age e meu nome é $name!")
+// Podemos adicionar parametros default na function
+fun cadastroDeUsuario(age: Int = 0, name: String, isAdmin: Boolean = false) {
+    println("Usuário cadastrado com idade: $age, nome: $name, admin: $isAdmin")
 }
